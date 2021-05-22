5587f7f873c5db28a78c4a6a3cdba68dae8b631c
Vinicius Dias <vinicius.dias@achievers.com>
2021-05-22 17:47:49 -0300

    Video 2.3

--- servidor-http.php
+++ servidor-http.php
@@ -1,12 +1,31 @@
 <?php
 
 use Swoole\Http\{Request, Response, Server};
+use Swoole\Coroutine\Http\Client;
 
 $servidor = new Server('0.0.0.0', 8080);
 
 $servidor->on('request', function (Request $request, Response $response) {
-    $response->header('Content-Type', 'text/html; charset=utf-8');
-    $response->end(print_r($request->header, true));
+    $channel = new chan(2);
+    go(function () use ($channel) {
+        $cliente = new Client('localhost', 8001);
+        $cliente->get('/servidor.php');
+
+        $conteudo = $cliente->getBody();
+        $channel->push($conteudo);
+    });
+
+    go(function () use ($channel) {
+        $conteudo = file_get_contents('arquivo.txt');
+        $channel->push($conteudo);
+    });
+
+    go(function () use ($channel, &$response) {
+        $primeiraResposta = $channel->pop();
+        $segundaResposta = $channel->pop();
+
+        $response->end($primeiraResposta . $segundaResposta);
+    });
 });
 
 $servidor->start();
